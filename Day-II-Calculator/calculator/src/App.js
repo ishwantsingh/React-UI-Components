954b506d75ec2591e6b986817657553d6801af0e
IshwantSingh <ishwant8@gmail.com>
2018-11-06 01:15:20 +0530

    calculator made. day2 work done

--- Day-II-Calculator/calculator/src/App.js
+++ Day-II-Calculator/calculator/src/App.js
@@ -1,28 +1,52 @@
 import React from 'react';
 import './App.css';
 import NumberButton from '../src/components/ButtonComponents/NumberButton'
+import ActionButton from '../src/components/ButtonComponents/ActionButton'
+import CalculatorDisplay from '../src/components/DisplayComponents/CalculatorDisplay'
 
 const App = () => {
   return (
-    <div>
-      <div className="NumberButton">
-            <NumberButton text={1} />
-            <NumberButton text={2} />
-            <NumberButton text={3} />
-            <NumberButton text={4} />
-            <NumberButton text={5} />
-            <NumberButton text={6} />
-            <NumberButton text={7} />
-            <NumberButton text={8} />
-            <NumberButton text={9} />
-      </div>
-      <div>
+    <div className="calculator">
 
+      <div className='top-look'>
+        <CalculatorDisplay />
+       </div>
+       <div className="all-buttons">
+        <div className="not-operators">
+          <div className="action-button">   
+            {/* <ActionButton text={4} buttonStyle={'actionLook zero'}/> */} 
+            <ActionButton text={'Clear'} buttonStyle={'actionLook clear'}/>
+          </div>
+
+          <div className="number-button">
+            <NumberButton text={7} buttonStyle={'button-look'}/>
+            <NumberButton text={8} buttonStyle={'button-look'}/>
+            <NumberButton text={9} buttonStyle={'button-look'}/>
+            <NumberButton text={4} buttonStyle={'button-look'}/>
+            <NumberButton text={5} buttonStyle={'button-look'}/>
+            <NumberButton text={6} buttonStyle={'button-look'}/>
+            <NumberButton text={1} buttonStyle={'button-look'}/>
+            <NumberButton text={2} buttonStyle={'button-look'}/>
+            <NumberButton text={3} buttonStyle={'button-look'}/>
+            <NumberButton text={0} buttonStyle={'button-look zero'}/>
+          </div>
+        </div>
+        <div>
+        <NumberButton text={'÷'} buttonStyle={'button-look operator'}/>
+        <NumberButton text={'X'} buttonStyle={'button-look operator X'}/>
+        <NumberButton text={'-'} buttonStyle={'button-look operator minus'}/>
+        <NumberButton text={'+'} buttonStyle={'button-look operator'}/>
+        <NumberButton text={'='} buttonStyle={'button-look operator'}/>
+        </div>
       </div>
+     
     </div>
   );
 };
 
-NumberButton();
+ActionButton;
+NumberButton;
+
+
 
 export default App;
--- Day-II-Calculator/calculator/src/components/ButtonComponents/NumberButton.js
+++ Day-II-Calculator/calculator/src/components/ButtonComponents/NumberButton.js
@@ -1,12 +1,6 @@
 import React from 'react';
 import './Button.css';
 
-// const NumberButton = () => {
-//     return (
-        
-//     );
-// }
-
 function NumberButton(props) {
     return (
         <button className={props.buttonStyle}>{props.text}</button>
