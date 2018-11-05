295e721c2b6d709b5635f525c616e98a71c7485b
Ishwant Singh <ishwant2001@gmail.com>
2018-11-05 12:08:35 +0530

    initial commit

--- Day-II-Calculator/calculator/src/components/ButtonComponents/NumberButton.js
+++ Day-II-Calculator/calculator/src/components/ButtonComponents/NumberButton.js
@@ -1,2 +1,16 @@
 import React from 'react';
 import './Button.css';
+
+// const NumberButton = () => {
+//     return (
+        
+//     );
+// }
+
+function NumberButton(props) {
+    return (
+        <button className={props.buttonStyle}>{props.text}</button>
+    );
+}
+
+export default NumberButton;
